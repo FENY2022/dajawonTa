614c4a9b0efefc6fd8823df059ede24c981092cf
FENY2022 <112142042+FENY2022@users.noreply.github.com>
2025-11-07 04:23:01 +0800

    da

--- dajawonta/content/provider_booking_details.php
+++ dajawonta/content/provider_booking_details.php
@@ -8,7 +8,7 @@ define('PAYMONGO_SECRET_KEY', 'sk_test_96buPr5S5wCEGUSjiEizVgMx'); // ✅ Use SE
 // NOTE: I've updated the SITE_BASE_URL to point to the base dashboard path,
 // but the success/cancel URLs will need to be correctly configured for your environment.
 // define('SITE_BASE_URL', 'http://localhost/dajawonTa/dajawonta/'); 
-define('SITE_BASE_URL', 'http://localhost/dajawonTa/dajawonta/'); 
+define('SITE_BASE_URL', 'http://dajawonta.online'); 
 
 
 // Enable detailed error display for debugging (only for UAT)
