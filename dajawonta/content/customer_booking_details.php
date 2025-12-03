9219d55d609f562a11e3830397519fe39a21a867
FENY2022 <112142042+FENY2022@users.noreply.github.com>
2025-12-03 11:52:32 +0800

    Update customer_booking_details.php

--- dajawonta/content/customer_booking_details.php
+++ dajawonta/content/customer_booking_details.php
@@ -212,29 +212,7 @@ function get_payment_badge_class($status) {
     </style>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
-        <div class="container">
-            <a class="navbar-brand" href="#">
-                <i class="fas fa-calendar-check me-2"></i>DajawonTa
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="my_bookings.php"><i class="fas fa-list me-1"></i> My Bookings</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="profile.php"><i class="fas fa-user me-1"></i> Profile</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="logout.php"><i class="fas fa-sign-out-alt me-1"></i> Logout</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+
 
     <div class="container py-4">
         <div class="d-flex justify-content-between align-items-center mb-4">
